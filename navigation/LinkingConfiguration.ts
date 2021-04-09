8424c944c9f2aee0166b4cec5a1943e9520f9d12
unknown <viviannc@gmail.com>
2021-04-09 18:51:20 -0300

    feat: url add 13-game

--- navigation/LinkingConfiguration.ts
+++ navigation/LinkingConfiguration.ts
@@ -1,11 +1,11 @@
 import * as Linking from "expo-linking"
 
 export default {
-  prefixes: [Linking.makeUrl("/13-game")],
+  prefixes: [Linking.makeUrl("/")],
   config: {
     screens: {
-      StartScreen: "start",
-      SelectTableScreen: "mesas",
+      StartScreen: "13-game/start",
+      SelectTableScreen: "13-game/mesas",
       SelectPlayersScreen: "jogadores/:mesa",
       SelectGameScreen: "game/type/:id",
       OrderPlayersScreen: "order",
